459e3683552d006f78d21d11afae1712dcb40295
Alejandro Ponce Mendoza <aponce@conabio.gob.mx>
2019-08-13 23:20:09 -0500

    put all in snake_style

--- R/History_PCA.R
+++ R/History_PCA.R
@@ -12,17 +12,18 @@ library(ggrepel)
 
 #Funcion con 4 componentes del PCA
 # datos = con la base de datos con sólo valores numericos
-# Variables = la variable categ´órica
-#Titulo = Es el titulo de la grafica
+# variables = la variable categórica
+#titulo = Es el titulo de la grafica
 #Multiplicador =  un valor para re-escalar la gráfica
-PCbiplot <- function(datos, Variables, Titulo, multiplicador){
+
+pca_biplot <- function(datos, variables, titulo, multiplicador){
   #pca1
-  pca1 <- dudi.pca(datos, scannf = F, nf = 3)
+  pca1 <- dudi.pca(as.data.frame(datos), scannf = F, nf = 3)
 
   pca1a <- data.frame(pca1$li)
   pca1b <- data.frame(pca1$co)
-  Variables <- as.factor(Variables)
-  Titulo <- as.character(c(Titulo))
+  variables <- as.factor(variables)
+  titulo <- as.character(c(titulo))
   Eigen <- pca1$eig
   PC1 <- round(100*(Eigen/sum(Eigen)),1)[1]
   PC2 <- round(100*(Eigen/sum(Eigen)),1)[2]
@@ -31,11 +32,11 @@ PCbiplot <- function(datos, Variables, Titulo, multiplicador){
     theme_classic() +
     geom_hline(yintercept = 0, color = "gray70") +
     geom_vline(xintercept = 0, color = "gray70") +
-    geom_point(aes(color = Variables), alpha = 0.7, size = 3) +
+    geom_point(aes(color = variables), alpha = 0.7, size = 3) +
     xlab(paste(PC1, "%", sep = " ")) +
     ylab(paste(PC2, "%", sep = " ") ) +
     #xlim(-5, 6) +
-    ggtitle(Titulo)
+    ggtitle(titulo)
 
   p2 <- p1 +
     geom_point(data = pca1b, aes(x = multiplicador*Comp1, y = multiplicador*Comp2), alpha = 0) +
@@ -46,19 +47,21 @@ PCbiplot <- function(datos, Variables, Titulo, multiplicador){
   p2
 }
 
-#PCbiplot(pca1$li, pca1$co, Tabla3$Var1$Grupos, pca1$eig,"PCA Grupos de K-means", 4)
+#pca_biplot(pca1$li, pca1$co, Tabla3$Var1$Grupos, pca1$eig,"PCA Grupos de K-means", 4)
 #
 
 #ejemplo con la base de datos iris
 iris
 head(iris)
 iris1 <- list(var = iris[,1:4], value = iris[,5])
-PCbiplot(iris1$var, iris1$value, "Hola", 2)
+pca_biplot(iris1$var, iris1$value, "Hola", 2)
 
+pca1 <- dudi.pca(as.data.frame(iris1$var), scannf = F, nf = 3)
+summary(pca1)
+
+pca1$Axis1
 #Ahora con mtcars
 head(mtcars)
 mtcars1 <- list(var = mtcars[,-10], value = mtcars[,10])
 
-PCbiplot(mtcars1$var, mtcars1$value, "Hola :)", 2 )
-
-
+pca_biplot(mtcars1$var, mtcars1$value, "Hola :)", 2 )
